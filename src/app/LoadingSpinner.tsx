7f9e271905bb93762a82673910d6ba3912541b4f
Will Doenlen <wdoenlen@gmail.com>
2024-02-18 20:45:17 -0500

    Trivial change to force a deploy

--- src/app/LoadingSpinner.tsx
+++ src/app/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center">
-      <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-blue-600"></div>
+      <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-blue-500"></div>
     </div>
   );
 }
